21d2b0b7010654436736beef6ac9d31d28aaff43
fabiomorais <fabio.jorge@gmail.com>
2021-02-12 15:10:16 -0300

    Iniciando repositório com código base

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+insert into produto
+values(10001,'Leite Integral', '87654321-B', 'Parmalat', FALSE, 'Mercearia', 4.5);
+insert into produto
+values(10002,'Arroz Integral', '87654322-B', 'Tio Joao', FALSE, 'Perecíveis', 5.5);
+insert into produto
+values(10003,'Sabao em Po', '87654323-B', 'OMO', FALSE, 'Limpeza', 12);
+insert into produto
+values(10004,'Agua Sanitaria', '87654324-C', 'Dragao', FALSE, 'limpesa', 3);
+insert into produto
+values(10005,'Creme Dental', '87654325-C', 'Colgate', FALSE, 'HIGIENE', 2.5);
