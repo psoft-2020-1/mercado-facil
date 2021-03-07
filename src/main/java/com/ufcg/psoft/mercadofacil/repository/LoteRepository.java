10325b1031902a65b9b192b82459e1d1a4c58c4c
IgorSerodio <igorserodioqueiroz@gmail.com>
2021-03-07 17:22:55 -0300

    Branch limpa

--- src/main/java/com/ufcg/psoft/mercadofacil/repository/LoteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ufcg.psoft.mercadofacil.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ufcg.psoft.mercadofacil.model.Lote;
-
-public interface LoteRepository extends JpaRepository<Lote, Long>{
-}
